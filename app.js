f345005ef490edfd46e91a7f43cf58d891af80e9
Pradip Chand <bhupendrachand749@gmail.com>
2025-05-01 09:50:35 +0545

    day11

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const express= require ("express")
 const app= express();
-const db= require("./Database/connect")
 
-app.get('/books',(req,res)=>{
+const {books}= require("./Database/connect")
+app.use(express.json())
+app.get('/books',async(req,res)=>{
+
+const datas=await books.findAll()
 
     res.json({
-        message:"Book is fetch or get Successfully."
+        message:"Book is fetch or get Successfully.",
+        data:datas
     })
 }                                                         
 );
+
+
 
 // //// database connection link 
 // "postgresql://postgres.cxqgmsppjjmtlvghyktz:[venom&@321]@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
 
-app.post('/books',(req,res)=>{
+app.post('/books',async(req,res)=>{
+console.log(req.body)
+const{bookName,bookPrice,bookAuthor,bookGenre}= req.body
+ await books.create({
+    bookName:bookName,
+    bookPrice:bookPrice,
+    bookAuthor:bookAuthor,
+    bookGenre:bookGenre
 
+})
     res.json({
-        message:"Book is Sucessfully Inserted."
+        message:"Book is Sucessfully Inserted.",
+       
     })
 }
 );
